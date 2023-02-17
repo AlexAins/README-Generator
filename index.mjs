5175fa2baa7e79116e2d8234973d886ec13e57d8
Alex Ainslie <alex.ainslie99@gmail.com>
2023-02-17 14:52:23 +0000

    Added basic skeleton for generated README and extra inputs

--- index.mjs
+++ index.mjs
@@ -5,32 +5,69 @@ const response = await inquirer
     .prompt([
         {
             type: "input",
-            name: "title",
+            name: "titleInput",
             message: "What is the Title of your Project?",
         },
         {
             type: "input",
-            name: "description",
-            message: "Provide a description of your Project",
+            name: "descriptionInput",
+            message: "Provide a description for your Project",
         },
         {
             type: "input",
-            name: "installation",
+            name: "installationInput",
             message: "Provide installation instructions for your Project",
         },
         {
             type: "input",
-            name: "usage",
+            name: "usageInput",
             message: "Provide usage information for your Project",
         },
         {
             type: "input",
-            name: "contribution",
+            name: "contributionInput",
             message: "What are the contribution guidelines for your Project?",
         },
         {
             type: "input",
-            name: "test",
+            name: "testInput",
             message: "What are the test instructions for your Project?",
         },
+        {
+            type: "input",
+            name: "githubInput",
+            message: "What is your GitHub Username?",
+        },
+        {
+            type: "input",
+            name: "emailInput",
+            message: "What is your email address?",
+        },
 ])
+
+let readmeTemplate =`
+#${response.titleInput}
+
+##Description
+
+
+##Table of Content
+
+
+##Installation
+
+
+##Usage
+
+
+##License
+
+
+##Contributing
+
+
+##Tests
+
+
+##Questions
+`
